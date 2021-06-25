8695119f85fec881fed691a5fdd3417fd6dff9c2
Rashid1267 <rgundlurmahamed1267@conestogac.on.ca>
2021-06-25 11:33:04 -0400

    dbjs

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,11 +6,10 @@ const connectDatabase = () => {
     mongoose.connect(process.env.DB_LOCAL_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        // dbName: 'capstoneDB'
         useCreateIndex: true
     }).then(con => {
         console.log(`MongoDB Database connected with host: ${con.connection.host}`)
     })
 }
 
-module.exports = connectDatabase 
+module.exports = connectDatabase
