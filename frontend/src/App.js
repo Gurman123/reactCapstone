5a700fd8523cfdadf5163106087a0db3c10f6482
GurmanpreetkaurGrewal <ggurman20@gmail.com>
2021-07-17 20:03:16 -0400

    Set New Password Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,14 @@ import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Home from './components/Home'
 import UpdateProfile from './components/user/UpdateProfile';
+import UpdatePassword from './components/user/UpdatePassword';
+import ForgotPassword from './components/user/ForgotPassword';
+import NewPassword from './components/user/NewPassword';
 import ProductDetails from './components/product/ProductDetails';
 import Login from './components/user/Login';
 import Register from './components/user/Register';
 import Profile from './components/user/Profile';
+
 
 import ProtectedRoute from './components/route/ProtectedRoute';
 
@@ -32,9 +36,13 @@ function App() {
       
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
+      <Route path="/password/forgot" component={ForgotPassword} exact/>
+      <Route path="/password/reset/:token" component={NewPassword} exact/>
+
       <ProtectedRoute path="/me" component={Profile} exact />
       <ProtectedRoute path="/me/update" component={UpdateProfile} exact />
-
+      <ProtectedRoute path="/password/update" component={UpdatePassword} exact />
+      
       </div>
       <Footer/>
     </div>
